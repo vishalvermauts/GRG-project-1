7a83389368814d21da03e61b53f385a086f6c8c8
Vishal Verma <162090492+vishalvermauts@users.noreply.github.com>
2024-09-20 09:33:16 +1000

    pin 0

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P1)) {
+    if (input.pinIsPressed(TouchPin.P0)) {
         music.ringTone(784)
     } else {
         music.stopAllSounds()
